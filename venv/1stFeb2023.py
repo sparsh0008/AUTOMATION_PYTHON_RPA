4835cb6493cf5c8897911b3274467a67d63dd818
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2023-02-07 12:13:10 +0530

    WHATSAPP AUTOMATED USING PYTHON SELENIUM

--- venv/1stFeb2023.py
+++ venv/1stFeb2023.py
@@ -23,17 +23,17 @@ WhatsappWeb = "https://web.whatsapp.com/"
 driver = webdriver.Chrome(executable_path='chromedriver.exe')
 driver.maximize_window()
 driver.get(WhatsappWeb)
-time.sleep(60)
+time.sleep(20)
 
 # Search the Contact Number and Click the Contact to send the message
 searchContact = driver.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]')
 searchContact.send_keys(PhoneNumber, Keys.ENTER)
-time.sleep(10)
+time.sleep(5)
 
 # Enter the Message You want to send to that Person
 messageBox = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
 messageBox.send_keys(mess, Keys.ENTER)
-time.sleep(15)
+time.sleep(5)
 
 print("Message {1} is send to {0} ".format(PhoneNumber, mess))
 
