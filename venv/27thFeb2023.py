7b5aab592c9985c7d254f9c08ec960f3be120cc9
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2023-02-27 23:20:59 +0530

    SOCKETS SERVER SIDE

---
+++ venv/27thFeb2023.py
@@ -0,0 +1,29 @@
+# import urllib.parse
+# url = "http://www.ab.com:88/engineering/computer-science.html"
+# tpl = urllib.parse.urlparse(url)
+# print(tpl)
+# print("SCHEME - {0}".format(tpl.scheme))
+# print("PATH - {0} ".format(tpl.path))
+# print("NETLOC - {0} ".format(tpl.netloc))
+# print("PORT - {0} ".format(tpl.port))
+# print("PARAMS - {0} ".format(tpl.params))
+# print("QUERY - {0} ".format(tpl.query))
+# print("GET URL - {0} ".format(tpl.geturl()))
+
+
+import socket
+
+host = "localhost"
+port = 5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET , SOCK_STREAM - used for TCPIP
+s.bind((host, port))
+s.listen(1)
+
+c, addr = s.accept()
+print(str(addr))
+
+s.send(b"Hello how are you")
+msg = "I am Fine"
+c.send(msg.encode())
+c.close()
