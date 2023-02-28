e563a496fdd64b2f5fd31c3e1d4363f5e7bbbc5b
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2023-02-28 12:43:20 +0530

    SOCKETS SERVER SIDE

--- venv/27thFeb2023.py
+++ venv/27thFeb2023.py
@@ -19,6 +19,7 @@ port = 5000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET , SOCK_STREAM - used for TCPIP
 s.bind((host, port))
 s.listen(1)
+print("Server is Listening")
 
 c, addr = s.accept()
 print(str(addr))
