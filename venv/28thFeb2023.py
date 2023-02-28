ada37d869e7b432f37ef339d92683e17a98422ee
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2023-02-28 12:44:29 +0530

    SOCKETS CLIENT SIDE

---
+++ venv/28thFeb2023.py
@@ -0,0 +1,15 @@
+import socket
+
+host = "localhost"
+port = 5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+msg = s.recv(1024)
+
+while msg:
+    print("Message : " + msg.decode())
+    msg = s.recv(1024)
+
+s.close()
