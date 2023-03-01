da358ac4e8e5cb10f13ab15df1dd17932e703f1a
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2023-03-01 12:47:00 +0530

    SOCKETS SERVER and CLIENT TCP

---
+++ venv/27thFeb2023_TCP_SERVER.py
@@ -0,0 +1,30 @@
+# import urllib.parse
+# url = "http://www.ab.com:88/engineering/computer-science.html"
+# tpl = urllib.parse.urlparse(url)
+# print(tpl)
+# print("SCHEME - {0}".format(tpl.scheme))
+# print("PATH - {0} ".format(tpl.path))
+# print("NETLOC - {0} ".format(tpl.netloc))
+# print("PORT - {0} ".format(tpl.port))
+# print("PARAMS - {0} ".format(tpl.params))
+# print("QUERY - {0} ".format(tpl.query))
+# print("GET URL - {0} ".format(tpl.geturl()))
+
+# SERVER SIDE TCPIP
+import socket
+
+host = "localhost"
+port = 5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET , SOCK_STREAM - used for TCPIP
+s.bind((host, port))
+s.listen(1)
+print("Server is Listening")
+
+c, addr = s.accept()
+print(str(addr))
+
+c.send(b"Hello how are you")
+msg = "I am Fine"
+c.send(msg.encode())
+c.close()
